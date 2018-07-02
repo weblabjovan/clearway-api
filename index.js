b3eaad291829b3a4d98cac4acea435795f8b6d48
weblabjovan <jovan.sutic@weblab.co.rs>
2018-07-02 19:39:33 +0200

    path config in index file

--- index.js
+++ index.js
@@ -18,6 +18,15 @@ require('./routes/authRoutes')(app);
 require('./routes/routeRoutes')(app);
 require('./routes/uploadRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	const path = require('path');
+	app.get('*', (req,res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+};
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 	console.log('AUTH listening to the port ' + PORT);
